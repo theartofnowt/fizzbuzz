c0d347c9da18ab10a8167de75be925e312f89648
theartofnowt <michaelgordon.brown1@gmail.com>
2017-06-27 15:39:07 +0100

    Ryan and Michael's fizzbuzz pair up

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,3 +5,15 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
 end
+
+describe 'fizzbuzz' do
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "2" when passed 2' do
+    expect(fizzbuzz(2)).to eq '2'
+  end
+end
