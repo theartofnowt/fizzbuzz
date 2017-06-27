f32eba7f4c98da24c23eae820a6a36d8891abfaf
daviryan <daviryan@aol.com>
2017-06-27 14:57:40 +0000

    added test to rspec paired fizzbuzz with Ryan and Michael

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -17,3 +17,9 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(2)).to eq '2'
   end
 end
+
+describe 'fizzbuzz' do
+  it 'returns "-2" when passed -2' do
+    expect(fizzbuzz(-2)).to eq '-2'
+  end
+end
